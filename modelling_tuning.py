eac6a303276690603837a1ce93b3bafc0978db4a
dirautariii <indirautari04@gmail.com>
2025-12-23 00:30:31 +0800

    Update model tuning, dependency, dan artefak evaluasi (confusion matrix & feature importance)

--- modelling_tuning.py
+++ modelling_tuning.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import dagshub
 import mlflow
 import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+dagshub.init(
+    repo_owner="dirautariii",
+    repo_name="Membangun_model",
+    mlflow=True
+)
 
 df = pd.read_csv("dataset_preprocessing/train_clean.csv")
 
@@ -12,7 +20,11 @@ X = df.drop("NObeyesdad", axis=1)
 y = df["NObeyesdad"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
 )
 
 param_grid = {
@@ -40,11 +52,29 @@ with mlflow.start_run(run_name="random_forest_tuning"):
     acc = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average="weighted")
 
-    mlflow.log_params(grid.best_params_)
+    mlflow.log_param("n_estimators", grid.best_params_["n_estimators"])
+    mlflow.log_param("max_depth", grid.best_params_["max_depth"])
+
     mlflow.log_metric("accuracy", acc)
     mlflow.log_metric("f1_score", f1)
 
     mlflow.sklearn.log_model(best_model, "model")
+
+    disp = ConfusionMatrixDisplay.from_estimator(
+        best_model, X_test, y_test
+    )
+    plt.title("Confusion Matrix")
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+    plt.close()
+
+    feature_importance = pd.DataFrame({
+        "feature": X.columns,
+        "importance": best_model.feature_importances_
+    }).sort_values(by="importance", ascending=False)
+
+    feature_importance.to_csv("feature_importance.csv", index=False)
+    mlflow.log_artifact("feature_importance.csv")
 
     print("Best params:", grid.best_params_)
     print("Accuracy:", acc)
