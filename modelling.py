6e35b98b08d15bf8f33f3043fe7fa9e5a8e71c4d
dirautariii <indirautari04@gmail.com>
2025-12-22 23:50:22 +0800

    Menambahkan training model dan logging MLflow

---
+++ modelling.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+df = pd.read_csv("dataset_preprocessing/train_clean.csv")
+
+X = df.drop(columns=["NObeyesdad"])
+y = df["NObeyesdad"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+with mlflow.start_run(run_name="baseline_random_forest"):
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="weighted")
+
+    mlflow.log_param("model", "RandomForestClassifier")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
+
+    mlflow.sklearn.log_model(model, "model")
+
+    print("Accuracy:", acc)
+    print("F1-score:", f1)
